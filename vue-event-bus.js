36cb38c6576496c37ebef5f56d6f6bc5c5aee3a9
devneko <dotneet@gmail.com>
2017-02-24 15:57:13 +0900

    fix exposing a Vue instance.

--- vue-event-bus.js
+++ vue-event-bus.js
@@ -6,7 +6,12 @@
 		if (options && 'name' in options && options.name) {
 			vueEventBus.config.name = options.name
 		}
-    var bus = new Vue({})
+    var v = new Vue({})
+    var bus = {}
+    bus.$on = function () { v.$on.apply(v, arguments) }
+    bus.$off = function () { v.$off.apply(v, arguments) }
+    bus.$emit = function () { v.$emit.apply(v, arguments) }
+    bus.$once = function () { v.$once.apply(v, arguments) }
 		Object.defineProperty(Vue.prototype, vueEventBus.config.name, {
 			get: function () {
 				return bus

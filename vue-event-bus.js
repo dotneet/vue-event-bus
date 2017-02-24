6f14d872a543b2c4959eb69bd53867cbcc464f37
devneko <dotneet@gmail.com>
2017-02-25 06:30:29 +0900

    format

--- vue-event-bus.js
+++ vue-event-bus.js
@@ -1,22 +1,22 @@
 ;(function () {
 
-	var vueEventBus = {}
+  var vueEventBus = {}
   vueEventBus.config = {name: '$bus'}
   vueEventBus.install = function (Vue, options) {
-		if (options && 'name' in options && options.name) {
-			vueEventBus.config.name = options.name
-		}
+    if (options && 'name' in options && options.name) {
+      vueEventBus.config.name = options.name
+    }
     var v = new Vue({})
     var bus = {}
     bus.$on = function () { v.$on.apply(v, arguments) }
     bus.$off = function () { v.$off.apply(v, arguments) }
     bus.$emit = function () { v.$emit.apply(v, arguments) }
     bus.$once = function () { v.$once.apply(v, arguments) }
-		Object.defineProperty(Vue.prototype, vueEventBus.config.name, {
-			get: function () {
-				return bus
-			}
-		})
+    Object.defineProperty(Vue.prototype, vueEventBus.config.name, {
+      get: function () {
+        return bus
+      }
+    })
   }
 
   if (typeof exports == "object") {
